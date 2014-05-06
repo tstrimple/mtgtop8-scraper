d512c949e7ef3bb84e87ccb53a4aa4cca63ba623
Timothy Strimple <tim@tstrimple.com>
2014-05-06 16:15:34 -0700

    initial commit

---
+++ cache.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+var util = require('util');
+var request = require('request');
+
+module.exports = function(options) {
+  var format = options.format || './cache/%s.html';
+  var source = options.source || 'http://www.mtgtop8.com/event?e=%d'
+
+  return {
+    get: function(id, fn) {
+      var cache = util.format(format, id);
+      fs.readFile(cache, { encoding: 'utf8' }, fn);
+    },
+
+    exists: function(id) {
+      var cache = util.format(format, id);
+      return fs.existsSync(cache);
+    },
+
+    invalidate: function(id, fn) {
+      var cache = util.format(format, id);
+      fs.unlink(cache);
+    },
+
+    update: function(id, fn) {
+      var url = util.format(source, id);
+      var cache = util.format(format, id);
+      var r = request(url);
+
+      r.pipe(fs.createWriteStream(cache));
+      
+      r.on('end', function() {
+          fn();
+      });
+
+      r.on('close', function() {
+        fn();
+      });
+
+      r.on('error', function(err) {
+        console.log('error updating ', id); 
+        fn(err);
+      });
+    }
+
+  };
+}
