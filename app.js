d1ebacc7d1a696ed3bf6076771e23ba1f047fddc
James Wright <jamesw@gmail.com>
2014-08-09 22:38:32 -0500

    Commenting the main flow

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var scrape = require('./scrape');
 var fs = require('fs');
 var ids = [];
 var completeCount = 0;
-var total = 7500;
+var total = 7946; // 8/8/2014 (Pro Tour M15)
 
 var ignoredIds = fs.readFileSync('./ignore.json');
 
+// Function to check if an object is in another object
 function contains(a, obj) {
     var i = a.length;
     while (i--) {
@@ -16,12 +17,15 @@ function contains(a, obj) {
     return false;
 }
 
+// Create an array of Ids from 1 to "total"
+// excluding any listed in "./ignore.json"
 for(var id = 1; id <= total; id++) {
   if(!contains(ignoredIds, id)) {
     ids.push(id);
   }
 }
 
+// Call "scrape" from "./scrape.js"
 scrape(ids, function(err) {
   console.log('done processing all files');
 });
